68ea60e17747c3aacbc6be6a013497b5b14a1fc4
tonogutierrez <antoniogtzmireles@gmail.com>
2025-03-29 21:57:26 -0600

    Fix ESLint React plugin issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
 import globals from "globals";
 import js from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: { globals: globals.browser },
-    plugins: { js },
+    plugins: { js, react: reactPlugin },
     rules: {
-      ...js.configs.recommended.rules, // Usa las reglas recomendadas
-      ...pluginReact.configs.recommended.rules, // Usa las reglas recomendadas de React
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
     },
   },
 ];
